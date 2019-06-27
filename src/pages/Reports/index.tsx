f287a2e679cf80e4312847a6cb2e0717dc7a4f45
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-06-27 09:06:26 +0200

    add socketjs

--- src/pages/Reports/index.tsx
+++ src/pages/Reports/index.tsx
@@ -28,6 +28,7 @@ import { RouterGlobalProps } from '../../models/router';
 import ReportsPage from '../../PresentationalComponents/ReportsPage';
 import LoadingState from '../../PresentationalComponents/LoadingState/LoadingState';
 import NoReports from '../../SmartComponents/NoReports';
+// import SockJS from 'sockjs-client';
 
 interface StateToProps {
     total: number;
@@ -45,10 +46,14 @@ interface Props extends StateToProps, DispatchToProps, RouterGlobalProps {
 
 interface State {
     columns: Array<ICell | String>;
-    rows: Array<IRow | Array<String>>
+    rows: Array<IRow | Array<String>>,
+    messages: any[]
 };
 
 export class Reports extends React.Component<Props, State> {
+
+    sock: any;
+    sock1: any;
 
     constructor(props: Props) {
         super(props);
@@ -64,7 +69,36 @@ export class Reports extends React.Component<Props, State> {
                 },
                 ''
             ],
-            rows: []
+            rows: [],
+            messages: []
+        };
+
+        // //create a new socket connection
+        // //see documentation https://github.com/sockjs/sockjs-client#getting-started
+        // const sock = new SockJS('/api/xavier/report-websocket');
+        // // const sock = new SockJS('wss://ci.foo.redhat.com:1337/api/xavier/report-websocket');
+
+        // sock.onopen = () => {
+        //     console.log('connection to server open');
+        // };
+
+        // sock.onmessage = (e: any) => {
+        //     this.setState({ messages: [ e.data, ...this.state.messages ]});
+        // };
+
+        // sock.onclose = () => {
+        //     console.log('close');
+        // };
+
+        // this.sock = sock;
+
+        this.sock1 = new WebSocket('ws://ci.foo.redhat.com:1337/api/xavier/report-websocket');
+        this.sock1.onmessage = (message: any) => {
+            console.log(message);
+        };
+
+        this.sock1.onclose = () => {
+            console.log('close sock1');
         };
     }
 
